7fa67f1664cae582c4c824a189b8868239a8e138
zoneksxd <147793154+zoneksxd@users.noreply.github.com>
2023-10-13 10:28:08 +0500

    Add files via upload

---
+++ pisya.js
@@ -0,0 +1,49 @@
+// let number = 9;
+
+// if (number < 0) {
+//     console.log("Число отрицательное");
+// } else {
+//     console.log("Число не отрицательное");
+// }
+
+// let str = "Я люблю разработку мобильных приложений";
+// console.log (str.length)
+
+// let str = "Я очень люблю разработку мобильных приложений";
+// let str2 = str.substring(str.length -1);
+// console.log(str2);
+
+// let a = 10
+// if (a % 2 === 0){
+//     console.log("чет")
+// }
+// else{
+//     console.log('нечет')
+// }    
+
+
+
+
+// function checkFirstLetters(word1, word2) {
+//   return word1[0].toLowerCase() === word2[0].toLowerCase();
+// }
+// console.log(checkFirstLetters('negro', 'nigger')); 
+// console.log(checkFirstLetters('deep', 'cock')); 
+// console.log(checkFirstLetters('Fingers', 'Food')); 
+// console.log(checkFirstLetters('Срун', 'Прун'));
+
+// let a= 721
+// let str=a.toString();
+// let b= parseInt(str[0]);
+// let c= parseInt(str[str.length -1]);
+// let d = b + c;
+// console.log(d)
+
+// let a = 1488;
+// let b = 0;
+
+// while(a > 0){
+//     b += a % 10
+//     a = Math.floor(a/10);
+// }
+// console.log(a)
